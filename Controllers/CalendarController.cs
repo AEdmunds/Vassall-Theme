d2a2c9613996ff0d08cb00eb2a75c2c928842a74
Alistair Edmunds <alistair.edmunds@gmail.com>
2016-09-07 16:55:42 +0100

    fixing style new name

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+
+namespace Controllers {
+    public class CalendarController : Controller{
+
+        
+        public JsonResult Index(string mes, string ano) {
+
+            var results = new {
+                items = new dynamic[] {
+                    new {
+                        date = string.Format("27/{0}/{1}",mes, ano),
+                        title = "Getting Contacts Barcelona",
+                        link = "http://gettingcontacts.com/events/view/barcelona"
+                    },
+                    new {
+                        date = string.Format("7/{0}/{1}",mes, ano),
+                        title = "2º Getting Contacts Alaquás",
+                        link = "http://gettingcontacts.com/events/view/alaquas",
+                        color = "#3B4",
+                        htmlPopup = "contingut popover"
+                    },
+                    new {
+                        date = string.Format("17/{0}/{1}",mes, ano),
+                        title ="4º Getting Contacts Barberà",
+                        link = "#",
+                        color = "#3B4",
+                        htmlPopup = "contingut popover"
+                    }
+                }
+            };
+
+
+            return Json(results,  JsonRequestBehavior.AllowGet);
+
+        }
+
+
+    }
+}
